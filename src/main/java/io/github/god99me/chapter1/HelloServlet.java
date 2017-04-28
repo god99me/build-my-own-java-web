acae42318d99c822f9b58df6567353fdabd2f22c
Liu Mingyang <343936465@qq.com>
2017-04-28 09:26:26 +0800

    build project develop environment

---
+++ src/main/java/io/github/god99me/chapter1/HelloServlet.java
@@ -0,0 +1,30 @@
+package io.github.god99me.chapter1;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * Created by L.M.Y on 2017/4/27.
+ */
+// 通过 WebServlet 达成零配置 web.xml
+@WebServlet("/hello")
+public class HelloServlet extends HttpServlet{
+
+    // control + N 自动生成提示
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        DateFormat dataFormat = new SimpleDateFormat("yyyy-mm-dd hh:mm:ss");
+        String currentTime = dataFormat.format(new Date());
+        req.setAttribute("currentTime", currentTime); // servlet - jsp hook
+        req.getRequestDispatcher("/WEB-INF/jsp/hello.jsp").forward(req, resp);
+        // debug 模式下运行，可以实现类的热部署以及断点调试
+        // 对于社区版可能需要多一些配置达到相同的效果
+    }
+}
---
+++ src/main/webapp/WEB-INF/jsp/hello.jsp
@@ -0,0 +1,18 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: L.M.Y
+  Date: 2017/4/27
+  Time: 下午11:24
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Hello Servlet</title>
+</head>
+<body>
+    <h1>Hello Servlet 3.0 :)</h1>
+
+    <p>当前时间： ${currentTime}</p>
+</body>
+</html>
